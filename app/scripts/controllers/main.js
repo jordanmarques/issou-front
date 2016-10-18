63b5403207a6f0c33269e9481644dd704c44fd4f
Jordan Marques <jordanmarques1994@gmail.com>
2016-10-18 10:24:18 +0200

    add procfile

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,15 +9,17 @@
  */
 angular.module('issouFrontApp')
   .controller('MainCtrl', function () {
+    var wallpaperUrl = "https://issou-randomizer.herokuapp.com/issou/wallpaper/random";
+    var iconUrl = "https://issou-randomizer.herokuapp.com/issou/icon/random";
 
-    this.wallpaperUrl = "http://localhost:8080/issou/wallpaper/random" + timeStamp();
-    this.iconUrl = "http://localhost:8080/issou/icon/random" + timeStamp();
+    this.wallpaperUrl = wallpaperUrl + timeStamp();
+    this.iconUrl = iconUrl + timeStamp();
 
     this.random = function(type){
       if(type == "wallpaper"){
-        this.wallpaperUrl = "http://localhost:8080/issou/wallpaper/random" + timeStamp();
+        this.wallpaperUrl = wallpaperUrl + timeStamp();
       }else if (type == "icon"){
-        this.iconUrl = "http://localhost:8080/issou/icon/random" + timeStamp();
+        this.iconUrl = iconUrl + timeStamp();
       }
     };
 
